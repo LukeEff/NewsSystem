e76c958a6879f6660ddb770d5820fd30a6a91792
lukeeff <luke4nandez@gmail.com>
2020-04-22 22:29:07 -0700

    Initial commit

---
+++ src/main/java/io/github/lukeeff/newssytem/NewsSystem.java
@@ -0,0 +1,17 @@
+package io.github.lukeeff.newssytem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NewsSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
